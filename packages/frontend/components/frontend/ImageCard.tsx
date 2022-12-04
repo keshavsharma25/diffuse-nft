a3b16d6a2a915d8f616770ab726983076b6306f8
Keshav <skeshav0825@gmail.com>
2022-12-04 10:33:18 +0530

    Create ImageCard.tsx

---
+++ packages/frontend/components/frontend/ImageCard.tsx
@@ -0,0 +1,14 @@
+import { Image, LinkBox, LinkOverlay } from "@chakra-ui/react";
+import React from "react";
+
+type Props = { imageUrl: string };
+
+export const ImageCard = (props: Props) => {
+  return (
+    <LinkBox>
+      <LinkOverlay>
+        <Image src={props.imageUrl} w="10rem" h="auto" />
+      </LinkOverlay>
+    </LinkBox>
+  );
+};
