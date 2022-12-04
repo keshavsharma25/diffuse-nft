366c10bc5cac4f6947305913e5ef68f540924e17
Keshav <skeshav0825@gmail.com>
2022-12-04 10:33:21 +0530

    Update index.tsx

--- packages/frontend/components/frontend/index.tsx
+++ packages/frontend/components/frontend/index.tsx
@@ -1,3 +1,5 @@
 export { OneClickHeading } from "./OneClickHeading";
 export { Navbar } from "./Navbar";
 export { Prompt } from "./Prompt";
+export { CustomDivider } from "./CustomDivider";
+export { ImageCard } from "./ImageCard";
