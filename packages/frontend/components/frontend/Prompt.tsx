31ea133d874a0a1c7216b20fb8d692981b1bdba9
Keshav <skeshav0825@gmail.com>
2022-12-04 10:33:27 +0530

    Update Prompt.tsx

--- packages/frontend/components/frontend/Prompt.tsx
+++ packages/frontend/components/frontend/Prompt.tsx
@@ -1,8 +1,27 @@
 import { Button, Flex, Input } from "@chakra-ui/react";
+import { useState } from "react";
 
-type Props = {};
+type Props = {
+  prompt: string;
+  setResponse: any;
+};
 
 export const Prompt = (props: Props) => {
+  const [prompt, setPrompt] = useState("");
+
+  const handlePrompt = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrompt(e.target.value);
+  };
+
+  const sendPrompt = async () => {
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      body: JSON.stringify({ prompt }),
+    });
+    const data = await response.json();
+    props.setResponse(data);
+  };
+
   return (
     <Flex alignItems={"center"} justifyContent="center" pt={"2rem"}>
       <Input
@@ -10,8 +29,11 @@ export const Prompt = (props: Props) => {
         size="lg"
         w="45%"
         focusBorderColor={"grey"}
+        onChange={handlePrompt}
       />
-      <Button size={"lg"}>Enter</Button>
+      <Button size={"lg"} type="submit" onClick={sendPrompt}>
+        Enter
+      </Button>
     </Flex>
   );
 };
