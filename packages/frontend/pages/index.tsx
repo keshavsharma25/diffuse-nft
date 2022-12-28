ed10e88a80432ad6e186a3d3f11224eecf5496bd
Keshav Sharma <skeshav0825@gmail.com>
2022-12-28 21:08:48 +0530

    Update index.tsx

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -6,60 +6,19 @@ import {
   Prompt,
 } from "@/components/frontend";
 import { Data } from "@/utils/types";
-import { Box, Heading, Skeleton } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Skeleton } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 
 export default function Home() {
   const { address, connector, isConnected } = useAccount();
 
-  const [showPrompt, setShowPrompt] = useState<Boolean>(false);
-  const [prompt, setPrompt] = useState<string>("");
-  const [dataResponse, setDataResponse] = useState<Data | null>(null);
-  const [isLoaded, setisLoaded] = useState<boolean>(false);
+  const [response, setResponse] = useState<Data | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleShowPrompt = () => {
-    setShowPrompt(true);
-  };
-
-  const handlePrompt = (prompt: string) => {
-    setPrompt(prompt);
-  };
-
-  const handleResponse = async (dataResponse: Data) => {
-    const id = dataResponse?.id;
-    const res = await (await fetch(`/api/check/${id}`)).json();
-    if (res.status === "success") {
-      setDataResponse(res);
-      setisLoaded(false);
-      return;
-    }
-    setDataResponse(res);
-    setisLoaded(true);
-  };
-
-  console.log(dataResponse);
-
-  const ImageCards = (): JSX.Element => {
-    if (dataResponse?.status === "success") {
-      return (
-        <Box>
-          {dataResponse?.items.map((item) => (
-            <ImageCard imageUrl={item} />
-          ))}
-        </Box>
-      );
-    }
-    return (
-      <Skeleton
-        height="100%"
-        width="100%"
-        borderRadius="lg"
-        startColor="gray.100"
-        endColor="gray.200"
-      />
-    );
-  };
+  useEffect(() => {
+    console.log("UseEffect: response is set to:", response);
+  }, [response]);
 
   return (
     <>
@@ -68,12 +27,18 @@ export default function Home() {
       <CustomDivider />
       {isConnected && (
         <Prompt
-          prompt={prompt}
-          setResponse={(data: Data) => setDataResponse(data)}
+          response={response}
+          setResponse={setResponse}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
         />
       )}
       <Box>
-        <ImageCards />
+        {response?.status === "succeeded"
+          ? response.items.map((item, index) => (
+              <ImageCard key={index} imageUrl={item} />
+            ))
+          : isLoading && <Skeleton width={"40rem"} height={"40rem"} />}
       </Box>
     </>
   );
