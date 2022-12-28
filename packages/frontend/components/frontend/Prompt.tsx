d46b260b90cb90b5541644e0791daa308643af3f
Keshav Sharma <skeshav0825@gmail.com>
2022-12-28 21:08:35 +0530

    Update Prompt.tsx

--- packages/frontend/components/frontend/Prompt.tsx
+++ packages/frontend/components/frontend/Prompt.tsx
@@ -1,26 +1,69 @@
+import { Data } from "@/utils/types";
 import { Button, Flex, Input } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Props = {
-  prompt: string;
-  setResponse: any;
+  response: Data | null;
+  setResponse: React.Dispatch<React.SetStateAction<Data | null>>;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  isLoading: boolean;
 };
 
-export const Prompt = (props: Props) => {
-  const [prompt, setPrompt] = useState("");
+export const Prompt = ({
+  response,
+  setResponse,
+  setIsLoading,
+  isLoading,
+}: Props) => {
+  const [prompt, setPrompt] = useState<string>("");
+  // const [id, setId] = useState<string>("");
 
   const handlePrompt = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPrompt(e.target.value);
   };
 
-  const sendPrompt = async () => {
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      body: JSON.stringify({ prompt }),
-    });
-    const data = await response.json();
-    props.setResponse(data);
+  const onSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
+    const resp = await (
+      await fetch("/api/generate", {
+        method: "POST",
+        body: JSON.stringify({ prompt }),
+      })
+    ).json();
+
+    console.log("set response:", resp);
+    console.log("set response id:", resp.id);
+
+    setIsLoading(true);
+    setResponse(resp);
   };
+
+  useEffect(() => {
+    console.log("useEffect: prompt is set to:", prompt);
+
+    if (response?.status === "created") {
+      const checkResponse = setInterval(async () => {
+        console.log("checkResponse id", response?.id);
+
+        const checkApiRes = await (
+          await fetch(`/api/check?id=${response?.id}`)
+        ).json();
+
+        if (checkApiRes.status === "succeeded") {
+          setIsLoading(false);
+          setResponse(checkApiRes);
+          clearInterval(checkResponse);
+        } else if (checkApiRes.status === "error") {
+          setIsLoading(false);
+          setResponse(checkApiRes);
+          clearInterval(checkResponse);
+        } else if (checkApiRes.status === "failed") {
+          setIsLoading(false);
+          setResponse(checkApiRes);
+          clearInterval(checkResponse);
+        }
+      }, 5000);
+    }
+  }, [response, prompt]);
 
   return (
     <Flex alignItems={"center"} justifyContent="center" pt={"2rem"}>
@@ -31,9 +74,15 @@ export const Prompt = (props: Props) => {
         focusBorderColor={"grey"}
         onChange={handlePrompt}
       />
-      <Button size={"lg"} type="submit" onClick={sendPrompt}>
-        Enter
-      </Button>
+      {!isLoading ? (
+        <Button size={"lg"} type="submit" onClick={onSubmit}>
+          Enter
+        </Button>
+      ) : (
+        <Button size={"lg"} type="submit" isLoading disabled>
+          Enter
+        </Button>
+      )}
     </Flex>
   );
 };
