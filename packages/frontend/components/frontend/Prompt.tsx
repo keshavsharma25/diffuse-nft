e45aba0bc3882970c46d286ccb2241e9a9acff61
Keshav Sharma <skeshav0825@gmail.com>
2022-12-30 03:22:13 +0530

    Update Prompt.tsx

--- packages/frontend/components/frontend/Prompt.tsx
+++ packages/frontend/components/frontend/Prompt.tsx
@@ -1,10 +1,30 @@
-import { Data } from "@/utils/types";
+import { ResponseType } from "@/utils/types";
 import { Button, Flex, Input } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+interface SDConfig {
+  cfg_scale: number;
+  clip_guidance_preset:
+    | "NONE"
+    | "FAST_BLUE"
+    | "FAST_GREEN"
+    | "NONE"
+    | "SIMPLE"
+    | "SLOW"
+    | "SLOWER"
+    | "SLOWEST";
+  height: number;
+  width: number;
+  samples: number;
+  seed: number;
+  steps: number;
+  prompt: string;
+  apiKey: string;
+}
 
 type Props = {
-  response: Data | null;
-  setResponse: React.Dispatch<React.SetStateAction<Data | null>>;
+  response: ResponseType | null;
+  setResponse: React.Dispatch<React.SetStateAction<ResponseType | null>>;
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
   isLoading: boolean;
 };
@@ -16,54 +36,67 @@ export const Prompt = ({
   isLoading,
 }: Props) => {
   const [prompt, setPrompt] = useState<string>("");
-  // const [id, setId] = useState<string>("");
 
   const handlePrompt = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPrompt(e.target.value);
   };
 
   const onSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
-    const resp = await (
-      await fetch("/api/generate", {
-        method: "POST",
-        body: JSON.stringify({ prompt }),
-      })
-    ).json();
-
-    console.log("set response:", resp);
-    console.log("set response id:", resp.id);
-
+    e.preventDefault();
     setIsLoading(true);
-    setResponse(resp);
-  };
 
-  useEffect(() => {
-    console.log("useEffect: prompt is set to:", prompt);
+    const body = {
+      cfg_scale: 7,
+      clip_guidance_preset: "FAST_BLUE",
+      height: 512,
+      width: 512,
+      samples: 2,
+      steps: 50,
+      prompt: prompt,
+      weight: 1,
+      apiKey: "sk-QRQpA5O40FeUG33OfENFxJqjyORAUSZXmrBpKmujbEF0faIs",
+    };
 
-    if (response?.status === "created") {
-      const checkResponse = setInterval(async () => {
-        console.log("checkResponse id", response?.id);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
 
-        const checkApiRes = await (
-          await fetch(`/api/check?id=${response?.id}`)
-        ).json();
+    try {
+      const res = await fetch("/api/retrieve_sd_images", options);
 
-        if (checkApiRes.status === "succeeded") {
-          setIsLoading(false);
-          setResponse(checkApiRes);
-          clearInterval(checkResponse);
-        } else if (checkApiRes.status === "error") {
-          setIsLoading(false);
-          setResponse(checkApiRes);
-          clearInterval(checkResponse);
-        } else if (checkApiRes.status === "failed") {
-          setIsLoading(false);
-          setResponse(checkApiRes);
-          clearInterval(checkResponse);
-        }
-      }, 5000);
+      if (!res.ok) {
+        console.error(`Error: ${res.status} - ${res.statusText}`);
+        return;
+      }
+
+      const status = res.status;
+      const data = await res.json();
+
+      console.log("Data:", data);
+
+      const finalResponse: ResponseType = {
+        status: status,
+        items: data.images,
+      };
+
+      setResponse(finalResponse);
+      console.log("Setting response to:", finalResponse);
+      setIsLoading(false);
+    } catch (error) {
+      if (error instanceof Error) {
+        const finalResponse: ResponseType = {
+          status: 500,
+          items: [],
+          error: error.message,
+        };
+        setResponse(finalResponse);
+      }
     }
-  }, [response, prompt]);
+  };
 
   return (
     <Flex alignItems={"center"} justifyContent="center" pt={"2rem"}>
