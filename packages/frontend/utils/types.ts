ace3c42893e725004c3e5a53a3125aa52c08cbea
Keshav Sharma <skeshav0825@gmail.com>
2022-12-30 03:23:02 +0530

    Update types.ts

--- packages/frontend/utils/types.ts
+++ packages/frontend/utils/types.ts
@@ -1,5 +1,31 @@
-export type Data = {
-  id?: string;
-  status: string;
-  items: string[];
-};
+export interface SDConfig {
+  cfg_scale: number;
+  clip_guidance_preset:
+    | "NONE"
+    | "FAST_BLUE"
+    | "FAST_GREEN"
+    | "NONE"
+    | "SIMPLE"
+    | "SLOW"
+    | "SLOWER"
+    | "SLOWEST";
+  height: number;
+  width: number;
+  samples: number;
+  seed: number;
+  steps: number;
+  prompt: string;
+  apiKey: string;
+}
+
+export interface ResponseType {
+  status: number;
+  items: ItemType[] | undefined;
+  error?: string;
+}
+
+export interface ItemType {
+  base64: string;
+  seed: number;
+  finishReason: string;
+}
