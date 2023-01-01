0333fbfabe56f7486f96f8f23af932681c6e9e9e
Keshav Sharma <skeshav0825@gmail.com>
2023-01-02 02:45:31 +0530

    Update ImageCard.tsx

--- packages/frontend/components/frontend/ImageCard.tsx
+++ packages/frontend/components/frontend/ImageCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 
 type Props = { base64: string };
 
@@ -6,7 +6,13 @@ export const ImageCard = ({ base64 }: Props) => {
   const url = `data:image/png;base64,${base64}`;
 
   return (
-    <Box>
+    <Box
+      maxW="sm"
+      borderWidth="1px"
+      borderRadius="xl"
+      overflow="hidden"
+      shadow="md"
+    >
       <Image src={url} alt="prompt image" />
     </Box>
   );
