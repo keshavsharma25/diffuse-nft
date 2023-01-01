68c8129d8f053066c3565aa8757da69daede480f
Keshav Sharma <skeshav0825@gmail.com>
2023-01-02 02:48:48 +0530

    Update index.tsx

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -5,17 +5,20 @@ import {
   OneClickHeading,
   Prompt,
 } from "@/components/frontend";
-import { Box, Skeleton } from "@chakra-ui/react";
+import { ResponseType } from "@/utils/types";
+import { Box, Flex, Skeleton } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
-import { ResponseType } from "@/utils/types";
 
 export default function Home() {
   const { address, connector, isConnected } = useAccount();
-
   const [response, setResponse] = useState<ResponseType | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [apiKey, setApiKey] = useState<string>("");
+  const [apiKey, setApiKey] = useState<string>(
+    process.env.NEXT_PUBLIC_STABILITY_API
+      ? process.env.NEXT_PUBLIC_STABILITY_API
+      : ""
+  );
 
   useEffect(() => {
     console.log("UseEffect: response is set to:", response);
@@ -33,9 +36,15 @@ export default function Home() {
           setResponse={setResponse}
           isLoading={isLoading}
           setIsLoading={setIsLoading}
+          apiKey={apiKey}
         />
       )}
-      <Box>
+      <Flex
+        flexFlow="row wrap"
+        my="5rem"
+        gap="1rem"
+        justifyContent="space-evenly"
+      >
         {!isLoading ? (
           response?.items?.map((item, index) => (
             <ImageCard key={index} base64={item.base64} />
@@ -43,7 +52,7 @@ export default function Home() {
         ) : (
           <Skeleton height="100vh" />
         )}
-      </Box>
+      </Flex>
     </>
   );
 }
