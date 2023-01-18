6ac45b23c32f840e3426ad7625fefb9404077188
Keshav Sharma <skeshav0825@gmail.com>
2023-01-18 16:16:18 +0530

    Fix- UI changes and Prop changes

--- packages/frontend/components/frontend/Prompt.tsx
+++ packages/frontend/components/frontend/Prompt.tsx
@@ -1,8 +1,17 @@
-import { ResponseType } from "@/utils/types";
-import { Box, Button, Flex, Input, useDisclosure } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
-import { useSettings } from "@/hooks";
+import { ResponseType, SDConfig } from "@/utils/types";
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  Input,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
 import { Settings } from "./Settings";
+import { BsCaretDown, BsCaretUp } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
 
 type Props = {
   response: ResponseType | null;
@@ -10,6 +19,13 @@ type Props = {
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
   isLoading: boolean;
   apiKey: string;
+  settings: SDConfig;
+  dispatch: React.Dispatch<{
+    type: string;
+    payload: any;
+  }>;
+  prompt: string;
+  setPrompt: React.Dispatch<React.SetStateAction<string>>;
 };
 
 export const SettingsContext = React.createContext({});
@@ -20,14 +36,18 @@ export const Prompt = ({
   setIsLoading,
   isLoading,
   apiKey,
+  settings,
+  dispatch,
+  prompt,
+  setPrompt,
 }: Props) => {
-  const [prompt, setPrompt] = useState<string>("");
   const { isOpen, onToggle } = useDisclosure();
-  const [settings, dispatch] = useSettings();
+
+  const promptRef = React.useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    console.log("UseEffect: settings is set to:", settings);
-  }, [settings]);
+    console.log("UseEffect: response is set to:", response);
+  }, [response]);
 
   const handlePrompt = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPrompt(e.target.value);
@@ -62,6 +82,12 @@ export const Prompt = ({
 
       if (!res.ok) {
         console.error(`Error: ${res.status} - ${res.statusText}`);
+        setIsLoading(false);
+        setResponse({
+          status: res.status,
+          items: [],
+          error: res.statusText,
+        });
         return;
       }
 
@@ -92,42 +118,142 @@ export const Prompt = ({
 
   return (
     <>
-      <Flex alignItems="center" justifyContent="center" pt="1rem" gap="0.5rem">
-        <Input
-          placeholder="Enter a prompt"
-          size="lg"
-          w="45%"
-          focusBorderColor={"grey"}
-          onChange={handlePrompt}
-        />
-        {!isLoading ? (
-          <Button
-            size="lg"
-            type="submit"
-            bgColor="black"
-            color="white"
-            onClick={onSubmit}
-          >
-            Enter
-          </Button>
-        ) : (
-          <Button size="lg" type="submit" isLoading disabled>
-            Enter
-          </Button>
-        )}
-        <Button
-          size="lg"
-          onClick={onToggle}
-          border="1px"
-          backgroundColor="white"
+      <Flex
+        flexDir={{
+          base: "column",
+          md: "row",
+        }}
+        justifyContent="center"
+        alignItems={{
+          base: "center",
+          md: "flex-start",
+        }}
+        mx={{
+          base: "3rem",
+          md: "0rem",
+        }}
+        gap={{
+          base: "1rem",
+          md: "0rem",
+        }}
+      >
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          columnGap="0.5rem"
+          flexDir="column"
+          w={{
+            base: "100%",
+            md: "50%",
+          }}
         >
-          Options
+          <Flex
+            border="1px solid gray"
+            w="100%"
+            {...(isOpen
+              ? { borderTopLeftRadius: "1rem" }
+              : { borderLeftRadius: "1rem" })}
+            overflow="hidden"
+            roundedTopRight={{
+              base: "1rem",
+              md: "0rem",
+            }}
+          >
+            <Input
+              placeholder="Enter a prompt"
+              width="100%"
+              onChange={handlePrompt}
+              border="none"
+              ref={promptRef}
+              _focus={{
+                outline: "none",
+                border: "none",
+                boxShadow: "none",
+              }}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  onSubmit(e as React.FormEvent<HTMLButtonElement>);
+                }
+              }}
+            />
+            {promptRef?.current?.value && (
+              <Button
+                backgroundColor="white"
+                _hover={{
+                  backgroundColor: "white",
+                }}
+                onClick={() => {
+                  if (promptRef?.current?.value) {
+                    promptRef.current.value = "";
+                    setPrompt("");
+                  }
+                }}
+              >
+                <RxCross2 color="rgba(0, 0, 0, 0.7)" />
+              </Button>
+            )}
+            {promptRef?.current?.value && (
+              <Center my="0.5rem">
+                <Divider orientation="vertical" borderColor="black" />
+              </Center>
+            )}
+            <Button
+              onClick={onToggle}
+              rounded="0rem"
+              backgroundColor="white"
+              _hover={{
+                backgroundColor: "white",
+              }}
+            >
+              {isOpen ? <BsCaretUp /> : <BsCaretDown />}
+            </Button>
+          </Flex>
+          <Box
+            width="100%"
+            borderX="1px solid gray"
+            {...(isOpen
+              ? {
+                  borderBottom: "1px solid gray",
+                  borderBottomRadius: "1rem",
+                }
+              : {})}
+          >
+            <Settings isOpen={isOpen} settings={settings} dispatch={dispatch} />
+          </Box>
+        </Flex>
+        <Button
+          bgColor="black"
+          width={{
+            base: "100%",
+            md: "50%",
+          }}
+          maxW="6rem"
+          py="1.313rem"
+          color="white"
+          onClick={onSubmit}
+          borderLeftRadius="0rem"
+          borderRightRadius="1rem"
+          _hover={{ bgColor: "rgba(0, 0, 0, 0.8)" }}
+          roundedLeft={{
+            base: "1rem",
+            md: "0rem",
+          }}
+          isLoading={isLoading}
+        >
+          Submit
         </Button>
       </Flex>
 
-      <Box>
-        <Settings isOpen={isOpen} settings={settings} dispatch={dispatch} />
-      </Box>
+      <Box></Box>
+      {/* <Box>
+        {response?.status != null && response?.status != 200 ? (
+          <Box>
+            <h1>Error: {response?.error}</h1>
+          </Box>
+        ) : (
+          ""
+        )}
+      </Box> */}
     </>
   );
 };
