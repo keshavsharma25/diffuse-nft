62ec1ec7acb8ddc94fb65d066c0913f20f41d3f5
Keshav Sharma <skeshav0825@gmail.com>
2023-01-18 16:14:21 +0530

    Fix- height width as select options

--- packages/frontend/components/frontend/OptionsSelect.tsx
+++ packages/frontend/components/frontend/OptionsSelect.tsx
@@ -24,6 +24,9 @@ export const OptionsSelect = ({
       dispatch({ type: "SET_CLIP_GUIDANCE_PRESET", payload: value });
     } else if (optionsTitle === "Sampler") {
       dispatch({ type: "SET_SAMPLER", payload: value });
+    } else if (optionsTitle === "Width x Height") {
+      dispatch({ type: "SET_WIDTH", payload: parseInt(value.split("x")[0]) });
+      dispatch({ type: "SET_HEIGHT", payload: parseInt(value.split("x")[1]) });
     } else {
       console.log("Error: OptionsSelect.tsx");
     }
@@ -38,12 +41,12 @@ export const OptionsSelect = ({
   };
 
   return (
-    <Flex flexFlow="column wrap" rowGap="1rem">
+    <Flex flexFlow="column wrap" rowGap="1rem" mx="1rem">
       <Text fontSize="1rem" fontWeight="semibold">
         {optionsTitle}
       </Text>
       <Select
-        size="sm"
+        size="xs"
         variant="filled"
         defaultValue={defaultValue}
         bgColor="whiteAlpha.900"
@@ -51,6 +54,12 @@ export const OptionsSelect = ({
         borderWidth="1px"
         borderRadius="lg"
         onChange={onChangeValue}
+        _hover={{
+          bgColor: "whiteAlpha.900",
+        }}
+        _focus={{
+          bgColor: "whiteAlpha.900",
+        }}
       >
         {selectValues.map((value, index) => {
           return <option key={index}>{value}</option>;
