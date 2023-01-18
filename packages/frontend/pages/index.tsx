da719a1743aca2ec007e84d9ef11aa6d3b81a7ac
Keshav Sharma <skeshav0825@gmail.com>
2023-01-18 16:18:31 +0530

    Feat- Added ImageCard & shifted prompt and settings state from prompt.tsx

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -5,8 +5,16 @@ import {
   OneClickHeading,
   Prompt,
 } from "@/components/frontend";
+import { useSettings } from "@/hooks";
 import { ResponseType } from "@/utils/types";
-import { Box, Flex, Skeleton } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Grid,
+  SimpleGrid,
+  Skeleton,
+  Spinner,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 
@@ -19,6 +27,8 @@ export default function Home() {
       ? process.env.NEXT_PUBLIC_STABILITY_API
       : ""
   );
+  const [prompt, setPrompt] = useState<string>("");
+  const [settings, dispatch] = useSettings();
 
   useEffect(() => {
     console.log("UseEffect: response is set to:", response);
@@ -27,7 +37,7 @@ export default function Home() {
 
   return (
     <>
-      <Navbar setApiKey={setApiKey} />
+      <Navbar setApiKey={setApiKey} apiKey={apiKey} />
       <OneClickHeading />
       <CustomDivider />
       {isConnected && (
@@ -37,22 +47,42 @@ export default function Home() {
           isLoading={isLoading}
           setIsLoading={setIsLoading}
           apiKey={apiKey}
+          settings={settings}
+          dispatch={dispatch}
+          prompt={prompt}
+          setPrompt={setPrompt}
         />
       )}
-      <Flex
-        flexFlow="row wrap"
-        my="5rem"
-        gap="1rem"
-        justifyContent="space-evenly"
-      >
-        {!isLoading ? (
-          response?.items?.map((item, index) => (
-            <ImageCard key={index} base64={item.base64} />
-          ))
-        ) : (
-          <Skeleton height="100vh" />
-        )}
-      </Flex>
+
+      {!isLoading ? (
+        <SimpleGrid
+          columns={{
+            base: 1,
+            sm: 1,
+            md: 2,
+            lg: 3,
+            xl: 4,
+          }}
+          spacing="10"
+          py="5rem"
+          px="5rem"
+        >
+          {response?.items?.map((item, index) => (
+            <ImageCard
+              key={index}
+              base64={item.base64}
+              prompt={prompt}
+              settings={settings}
+            />
+          ))}
+        </SimpleGrid>
+      ) : (
+        <Flex flexDir="row" py="10rem">
+          <Skeleton height="1.5rem" />
+          <Skeleton height="1.5rem" />
+          <Skeleton height="1.5rem" />
+        </Flex>
+      )}
     </>
   );
 }
