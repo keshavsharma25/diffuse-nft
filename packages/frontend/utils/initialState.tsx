0c839ac723d64ac5205a8f5185c27de4e5df14cf
keshavsharma25 <skeshav0825@gmail.com>
2023-02-05 20:30:44 +0530

    fix: ordered the initialSettingsState

--- packages/frontend/utils/initialState.tsx
+++ packages/frontend/utils/initialState.tsx
@@ -2,13 +2,12 @@ import { guidancePresetMap, samplerMap } from "@/components/frontend/Settings";
 import { SDConfig } from "./types";
 
 export const initialSettingsState: SDConfig = {
-  cfg_scale: 7,
-  clip_guidance_preset: guidancePresetMap.GUIDANCE_PRESET_NONE,
-  height: 512,
   width: 512,
-  sampler: samplerMap.SAMPLER_K_DPMPP_2M,
-  samples: 1,
+  height: 512,
   seed: 0,
   steps: 50,
-  weight: 1,
+  samples: 1,
+  cfg_scale: 7,
+  clip_guidance_preset: guidancePresetMap.GUIDANCE_PRESET_NONE,
+  sampler: samplerMap.SAMPLER_K_DPMPP_2M,
 };
